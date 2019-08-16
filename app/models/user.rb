b90e53adad89320b467f48cd2561e6e4a174d36d
K11810 <fastofall6545@gmail.com>
2019-08-16 13:52:22 +0000

    #15_【add】 validation in User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # validate
+  validates :name, presence: true, length: { in: 1..50 }
+  validates :icon, presence: true, length: { in: 1..255 }
+  validates :email,
+            presence: true,
+            length: { in: 1..255 },
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+            uniqueness: true
+
 end
