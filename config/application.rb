a25824cae762794eeb0a6fad0a60ec24f062fa08
K11810 <fastofall6545@gmail.com>
2019-08-16 05:56:11 +0000

    #13_【Change】 make lauguage in this application standarize Japanese.

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ module Casiopea
   class Application < Rails::Application
     config.load_defaults 5.2
 
+    config.i18n.default_locale = :ja
+
     config.generators do |g|
       g.test_framework :rspec,
         fixtures: true,
