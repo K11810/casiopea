f63971c5cf394c8860d29249bdb9656c4ecad958
K11810 <fastofall6545@gmail.com>
2019-08-17 01:30:25 +0000

    #17_【add】 devise configulation for user detail view.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+  # for CSRF measure
+  protect_from_forgery with: :exception
+
+  # permit only loginned users
+  before_action :authenticate_user!
+
+  # add strong parameters to devise controllers
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    # add strong parameter of 'name' in sign_up
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :icon])
+    # add strong parameters of 'name' and 'icon' in account edit
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :icon])
+  end
+
+  # move words/index after login
+  def after_sign_in_path_for(resource)
+    words_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'users/show'
   root 'words#index'
+
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
+  resources :users, only: [:show]
+
   resources :words do
   end
 end
