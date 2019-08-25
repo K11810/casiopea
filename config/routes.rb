bf004c9cd03c75e419cc2c26a054868b94c0666c
K11810 <fastofall6545@gmail.com>
2019-08-25 06:01:01 +0000

    #47_【add】 top and about pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  root 'words#index'
+
+  root 'homes#top'
+
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
   devise_for :users, controllers: {
     registrations: 'users/registrations',
   }
+
   resources :users, only: [:show]
+
+  get 'homes/about'
 
   resources :words do
   end
